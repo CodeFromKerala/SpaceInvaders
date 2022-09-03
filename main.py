8c61db4aba9e4d8296d4a86309855f3fc22d7f03
CodeFromKerala <mjnair52@gmail.com>
2022-09-03 14:45:29 +0530

    Changes

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 import pygame
 
 display = pygame.display.set_mode((500, 500))
+pygame.display.set_caption("Space Ship Game")
+clock = pygame.time.Clock()
+FPS = 60
 
 run = True
 while run:
+    clock.tick(FPS)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+    display.fill((255, 255, 255))
+    pygame.display.update()
