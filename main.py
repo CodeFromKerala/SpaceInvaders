0810c4697666337b962b3dc927a5f58a9a1ee315
CodeFromKerala <mjnair52@gmail.com>
2022-09-04 13:21:01 +0530

    Player and Enemy Class Added

--- main.py
+++ main.py
@@ -5,11 +5,28 @@ pygame.display.set_caption("Space Ship Game")
 clock = pygame.time.Clock()
 FPS = 60
 
-run = True
-while run:
-    clock.tick(FPS)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
-    display.fill((255, 255, 255))
-    pygame.display.update()
+class Player(pygame.Rect):
+    def __init__(self):
+        self.image = pygame.image.load("./player.png")
+    def update(self):
+        display.blit(self.image, self)
+
+class Enemy(pygame.Rect):
+    def __init__(self):
+        self.image = pygame.image.load("./enemy.png")
+    def update(self):
+        display.blit(self.image, self)
+
+
+def main():
+    run = True
+    while run:
+        clock.tick(FPS)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+        display.fill((255, 255, 255))
+        pygame.display.update()
+
+if __name__ == "__name__":
+    quit()
