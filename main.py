c383c0f57802cafcefe78334388b3499203edcc0
CodeFromKerala <mjnair52@gmail.com>
2022-09-07 15:17:24 +0530

    Fixed Running Bug

--- main.py
+++ main.py
@@ -1,32 +1,32 @@
+# importing pygame
 import pygame
 
+# screen loading and fps
 display = pygame.display.set_mode((500, 500))
 pygame.display.set_caption("Space Ship Game")
-clock = pygame.time.Clock()
-FPS = 60
 
+# Player Class
 class Player(pygame.Rect):
     def __init__(self):
         self.image = pygame.image.load("./player.png")
     def update(self):
         display.blit(self.image, self)
 
+# Enemy Class
 class Enemy(pygame.Rect):
     def __init__(self):
         self.image = pygame.image.load("./enemy.png")
     def update(self):
         display.blit(self.image, self)
 
-
+# Main game loop
 def main():
     run = True
     while run:
-        clock.tick(FPS)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-        display.fill((255, 255, 255))
-        pygame.display.update()
 
-if __name__ == "__name__":
+# check if the file is not executed by import
+if __name__ == "__main__":
     main()
